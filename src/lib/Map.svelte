9f9dd2676f9c63f1c366bc293b8ae9db6b9c4d9b
Mark Firman <markfirn@gmail.com>
2026-04-29 12:12:22 +0200

    Add more places

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -28,6 +28,7 @@
         { name: 'Maternity hospital', lat: 47.15182764443729, lng: 37.608116383840496, zoom: 19 },
         { name: 'History museum', lat: 47.0928687, lng: 37.5575726, zoom: 19 },
         { name: 'Kuindzhi museum', lat: 47.0934991, lng: 37.5507241, zoom: 19 },
+        { name: 'Azovstal steelworks', lat: 47.0925, lng: 37.6131, zoom: 14 },
         { name: 'Destroyed PortCity', lat: 47.11589505454007, lng: 37.50751135491948, zoom: 18 },
         { name: '350 unknown graves, some filled (Manhush)', lat: 47.073021957525, lng: 37.29967261999493, zoom: 18 },
         { name: 'Firing positions (Manhush)', lat: 47.07496052993682, lng: 37.35484725751761, zoom: 17 },
@@ -37,6 +38,19 @@
       city: 'Bakhmut',
       items: [
         { name: 'Intensive Care Hospital', lat: 48.59617006237138, lng: 37.99740525528709, zoom: 18 },
+        { name: 'Chasiv Yar', lat: 48.58799307992216, lng: 37.834246642208235, zoom: 18 },
+      ],
+    },
+    {
+      city: 'Maryinka',
+      items: [
+        { name: 'Kindergarten', lat: 47.934670821743985, lng: 37.50297129708892, zoom: 19 },
+      ],
+    },
+    {
+      city: 'Popasna',
+      items: [
+        { name: 'Popasna', lat: 48.63020819962778, lng: 38.373239418150774, zoom: 18 },
       ],
     },
     {
